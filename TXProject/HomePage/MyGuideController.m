191a747589503cdd2f6dd208512ebfc7abb16ba3
Sam <Sam@userdeMacBook-Pro-2.local>
2019-02-27 17:43:53 +0800

    差综合服务查询付款状态接口

--- TXProject/HomePage/MyGuideController.m
+++ TXProject/HomePage/MyGuideController.m
@@ -20,7 +20,8 @@
     [self.scrollview setContentSize:CGSizeMake(ScreenW, 5500*kScale)];
     UIImageView *imageV = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"guide_one"]];
     imageV.frame = CGRectMake(0, 0, ScreenW, 4500*320/ScreenW*kScale);
-    
+    ///使用指导
+
     [self.scrollview addSubview:imageV];
     
 }
